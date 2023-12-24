b258be973212230014d6ccc6e4307d0d912f7bfc
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-25 02:22:12 +0900

    feat: add disabled prop to Like

--- src/components/Like/Like.tsx
+++ src/components/Like/Like.tsx
@@ -5,14 +5,16 @@ interface Props {
   className?: string;
   active?: boolean;
   likes?: number;
+  disabled?: boolean;
 }
-const Like: React.FC<Props> = ({ active, className, likes = 0 }) => {
+const Like: React.FC<Props> = ({ active, className, likes = 0, disabled }) => {
   return (
     <p className={classNames(styles.Like, className)}>
       <button
         className={classNames(styles.Like__button, {
           [styles.Like__buttonActive]: active,
         })}
+        disabled={disabled}
       />
 
       <span className={styles.Like__counter}>{likes}</span>
