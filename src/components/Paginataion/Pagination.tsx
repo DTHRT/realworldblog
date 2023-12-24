d3c5a47d853a71a463032f4d201b971b32174346
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-25 00:13:10 +0900

    feat: add Pagination component

---
+++ src/components/Paginataion/Pagination.tsx
@@ -0,0 +1,27 @@
+import ReactPaginate from "react-paginate";
+import styles from "./Pagination.module.scss";
+
+interface Props {
+  pageCount: number;
+  handlePageClick: (event: any) => void;
+}
+const Pagination: React.FC<Props> = ({ pageCount, handlePageClick }) => {
+  return (
+    <ReactPaginate
+      className={styles.Pagination}
+      pageCount={pageCount}
+      previousLinkClassName={styles.Pagination__prev}
+      previousLabel=""
+      nextLinkClassName={styles.Pagination__next}
+      nextLabel=""
+      activeLinkClassName={styles.Pagination__activelink}
+      pageLinkClassName={styles.Pagination__link}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={0}
+      renderOnZeroPageCount={null}
+      onPageChange={handlePageClick}
+    />
+  );
+};
+
+export default Pagination;
