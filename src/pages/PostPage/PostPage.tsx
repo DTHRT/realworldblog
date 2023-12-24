3431bc623b192fe724615ebb91080afcb3ed05db
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-25 02:32:08 +0900

    feat: add PostPage.tsx

---
+++ src/pages/PostPage/PostPage.tsx
@@ -0,0 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import Loader from "../../components/Loader";
+import Post from "../../components/Post";
+import Api from "../../services/api";
+import PostList from "../../components/PostList";
+
+const PostPage = () => {
+  const { slug } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [article, setArticle] = useState(null);
+  const api = new Api();
+
+  useEffect(() => {
+    setLoading(true);
+    api.getPost(slug).then(({ article }) => {
+      setArticle(article);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <PostList>
+        {loading ? (
+          <Loader />
+        ) : (
+          article && <Post article={article} full={true} />
+        )}
+      </PostList>
+    </>
+  );
+};
+
+export default PostPage;
