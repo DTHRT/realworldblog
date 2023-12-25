4aa38161210b7363af96f1865540b90a732b30e3
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-25 23:36:38 +0900

    feat: upgrade Form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,34 @@
 import styles from "./Form.module.scss";
 
 interface Props {
+  title?: string;
   children: React.ReactNode;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  submitText?: string;
+  footerText?: React.ReactNode;
 }
 
-const Form: React.FC<Props> = ({ children }) => {
+const Form: React.FC<Props> = ({
+  title,
+  children,
+  onSubmit,
+  submitText,
+  footerText,
+}) => {
   return (
-    <div className={styles.Form}>
-      <h2 className={styles.Form__title}>Sign In</h2>
+    <form className={styles.Form} onSubmit={onSubmit}>
+      {title && <h2 className={styles.Form__title}>{title}</h2>}
 
-      <form className={styles.Form__form}>{children}</form>
-    </div>
+      <div className={styles.Form__form}>{children}</div>
+
+      <div className={styles.Form__footer}>
+        <button type="submit" className={styles.Form__submit}>
+          {submitText || "Submit"}
+        </button>
+
+        {footerText && <p className={styles.Form__footerText}>{footerText}</p>}
+      </div>
+    </form>
   );
 };
 
