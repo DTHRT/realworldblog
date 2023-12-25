d5c9ec4e3efa2bd8b5bb4030e01d6e10b3d9fd49
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-26 02:36:26 +0900

    feat: add Header style when user logged in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,42 @@
 import Button from "../Button";
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import UserBlock from "../UserBlock";
 
 const Header: React.FC = () => {
+  const user = useSelector((state: RootState) => state.user);
+  // const { email } = user;
+
+  const email = "qwe@qwe.com";
+
   return (
     <header className={styles.Header}>
       <h1 className="title">Real World Blog</h1>
-      <div className="buttons">
-        <Link to="/sign-in">
-          <Button>Sign In</Button>
-        </Link>
-        <Link to="/sign-up">
-          <Button variant="success">Sign Up</Button>
-        </Link>
+      <div className={styles.Header__buttons}>
+        {email ? (
+          <>
+            <Link to="/create-article">
+              <Button variant="create-article">Create article</Button>
+            </Link>
+
+            <UserBlock username="Dmitry" image="" />
+
+            <Link to="/logout">
+              <Button variant="black">Log Out</Button>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/sign-in">
+              <Button>Sign In</Button>
+            </Link>
+            <Link to="/sign-up">
+              <Button variant="success">Sign Up</Button>
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
