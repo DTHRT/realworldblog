92badd77ec910485aa2f0e9e85f9e0c1e3dd4001
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-25 16:37:05 +0900

    feat: inject Container to app

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,22 @@ import {
 import PostPage from "./pages/PostPage";
 import ArticlesPage from "./pages/ArticlesPage/ArticlesPage";
 import Header from "./components/Header";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
       <Header />
 
-      <Router>
-        <Switch>
-          <Route exact path="/" render={() => <Redirect to="/articles" />} />
-          <Route exact path="/articles" component={ArticlesPage} />
-          <Route exact path="/articles/:slug" component={PostPage} />
-        </Switch>
-      </Router>
+      <Container>
+        <Router>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/articles" />} />
+            <Route exact path="/articles" component={ArticlesPage} />
+            <Route exact path="/articles/:slug" component={PostPage} />
+          </Switch>
+        </Router>
+      </Container>
     </>
   );
 }
