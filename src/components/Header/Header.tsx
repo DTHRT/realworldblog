c2e9aacbe4137461f587cfd785c3007744f35e4c
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-28 15:59:46 +0900

    feat: add real data for Header and create logout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,38 @@
 import Button from "../Button";
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 import UserBlock from "../UserBlock";
+import { logout } from "../../features/user/userSlice";
 
 const Header: React.FC = () => {
   const user = useSelector((state: RootState) => state.user);
-  // const { email } = user;
+  const { token, username } = user;
 
-  const email = "qwe@qwe.com";
+  const dispatch = useDispatch();
+
+  const onLogOut = () => {
+    localStorage.removeItem("user");
+    dispatch(logout());
+  };
 
   return (
     <header className={styles.Header}>
       <h1 className="title">Real World Blog</h1>
       <div className={styles.Header__buttons}>
-        {email ? (
+        {token ? (
           <>
             <Link to="/create-article">
               <Button variant="create-article">Create article</Button>
             </Link>
 
-            <UserBlock username="Dmitry" image="" />
+            <UserBlock username={username} image="" />
 
-            <Link to="/logout">
-              <Button variant="black">Log Out</Button>
+            <Link to="/">
+              <Button variant="black" onClick={onLogOut}>
+                Log Out
+              </Button>
             </Link>
           </>
         ) : (
