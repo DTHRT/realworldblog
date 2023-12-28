d94bf4f6c6b37900d7c9fa6b30f4b605016af7be
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-12-28 15:58:53 +0900

    feat: add field username to userSlice.ts

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface UserState {
+  username: string;
   email: string;
   token: string | null;
 }
 
 const initialState: UserState = {
+  username: "",
   email: "",
   token: null,
 };
@@ -15,11 +17,18 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     login: (state, action) => {
+      state.username = action.payload.username;
       state.email = action.payload.email;
       state.token = action.payload.token;
     },
+
+    logout: (state) => {
+      state.username = "";
+      state.email = "";
+      state.token = null;
+    },
   },
 });
-export const { login } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 
 export default userSlice.reducer;
