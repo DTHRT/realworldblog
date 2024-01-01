6e644113414f657637de225e8880301e366bff62
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2024-01-01 18:19:57 +0900

    feat: add admin buttons to Post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -2,9 +2,13 @@ import styles from "./Post.module.scss";
 import Tag from "../Tag";
 import Like from "../Like";
 import classNames from "classnames";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Markdown from "react-markdown";
 import UserBlock from "../UserBlock";
+import Button from "../Button";
+import { useSelector } from "react-redux";
+import Api from "../../services/api";
+import { toast } from "react-toastify";
 
 interface Props {
   article: any;
@@ -22,6 +26,32 @@ const Post: React.FC<Props> = ({ article, full }) => {
     updatedAt,
   } = article;
   const { username, image } = author;
+  const { username: currentUsername, token } = useSelector(
+    (state: any) => state.user,
+  );
+  const api = new Api();
+  const history = useHistory();
+
+  const deleteArticle = async () => {
+    if (!token) {
+      return toast.error("Please sign in");
+    }
+
+    const response = await api.deletePost(slug, token);
+
+    const { errors } = response;
+
+    if (errors) {
+      const errorMessages = Object.entries(errors)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join(" ");
+
+      return toast.error(errorMessages);
+    }
+
+    toast.success("Article deleted successfully");
+    history.push("/");
+  };
 
   return (
     <article className={styles.Post}>
@@ -66,6 +96,30 @@ const Post: React.FC<Props> = ({ article, full }) => {
 
         <UserBlock username={username} image={image} date={updatedAt} />
       </header>
+
+      {full && currentUsername === username && (
+        <div className={styles.Post__myPost}>
+          <Button
+            className={classNames(
+              styles.Post__myPostButton,
+              styles.Post__myPostButtonDelete,
+            )}
+            onClick={deleteArticle}
+          >
+            Delete
+          </Button>
+          <Link to={`/articles/${slug}/edit`}>
+            <Button
+              className={classNames(
+                styles.Post__myPostButton,
+                styles.Post__myPostButtonEdit,
+              )}
+            >
+              Edit
+            </Button>
+          </Link>
+        </div>
+      )}
 
       {description && (
         <p
