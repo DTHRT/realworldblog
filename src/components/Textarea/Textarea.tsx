efa28b49510658c56fcbdab8181abb0f648ccadd
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2024-01-01 15:06:24 +0900

    feat: create Textarea component

---
+++ src/components/Textarea/Textarea.tsx
@@ -0,0 +1,39 @@
+import styles from "./Textarea.module.scss";
+import classNames from "classnames";
+
+interface Props {
+  label: string;
+  name: string;
+  placeholder: string;
+  register: any;
+  error?: any;
+}
+
+const Textarea: React.FC<Props> = ({
+  label,
+  name,
+  placeholder,
+  register,
+  error,
+}) => {
+  return (
+    <label htmlFor={`${name}`} className={styles.Textarea}>
+      <span className={styles.Textarea__label}>{label}</span>
+
+      <textarea
+        className={classNames(styles.Textarea__input, {
+          [styles.Textarea__input_error]: error,
+        })}
+        name={name}
+        placeholder={placeholder}
+        id={name}
+        {...register}
+        rows={10}
+      />
+
+      {error && <p className={styles.Textarea__error}>{error.message}</p>}
+    </label>
+  );
+};
+
+export default Textarea;
