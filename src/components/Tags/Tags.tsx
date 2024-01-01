78098250b2097c3dff9e35deb6e0f00fc7e1ab4c
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2024-01-01 15:06:05 +0900

    feat: create Tags component

---
+++ src/components/Tags/Tags.tsx
@@ -0,0 +1,72 @@
+import styles from "./Tags.module.scss";
+import InputText from "../InputText";
+import Button from "../Button";
+import { useFieldArray } from "react-hook-form";
+
+interface TagsProps {
+  register: any;
+  errors: any;
+  control: any;
+}
+
+const Tags: React.FC<TagsProps> = ({ register, errors, control }) => {
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "tags",
+  });
+
+  console.log(fields);
+
+  return (
+    <div className={styles.Tags}>
+      <span className={styles.Tags__label}>Tags</span>
+      <ul className={styles.Tags__list}>
+        {fields.map((field, index) => {
+          return (
+            <li className={styles.Tags__item} key={field.id}>
+              <InputText
+                placeholder="Tag"
+                name={`tag${index}`}
+                register={{
+                  ...register(`tags.${index}.tag`, {
+                    required: "Tag is required",
+                  }),
+                }}
+                error={errors.tags?.[index]?.tag}
+              />
+              <Button
+                className={styles.Tags__deleteBtn}
+                onClick={remove}
+                type="button"
+              >
+                Delete
+              </Button>
+
+              {fields.length - 1 === index && (
+                <Button
+                  className={styles.Tags__addBtn}
+                  onClick={() => append({ tag: "" })}
+                  type="button"
+                >
+                  Add tag
+                </Button>
+              )}
+            </li>
+          );
+        })}
+
+        {fields.length === 0 && (
+          <Button
+            className={styles.Tags__addBtn}
+            onClick={() => append({ tag: "" })}
+            type="button"
+          >
+            Add tag
+          </Button>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Tags;
